6eca81b02567ab4bd65437592d6007f28a614fe5
Oscar Muñoz G <oscarandres.munozgonzalez@gmail.com>
2020-08-18 21:42:47 -0400

    Generar hola mundo
    Generar estructura de proyecto android
    Agregar paleta de colores del proyecto
    Construir layouts base

---
+++ Fuentes/CalculadoraApp/app/src/main/java/cl/inacap/calculadoraApp/MainActivity.java
@@ -0,0 +1,22 @@
+package cl.inacap.calculadoraApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void saludar(View v){
+
+        Toast.makeText(this, "Hola mundo al Oscar", Toast.LENGTH_SHORT).show();
+
+    }
+}
